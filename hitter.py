b379707c078403c25cd3d542311b7bec65c064b0
Shalom Vanderhoof <shalomzev@gmail.com>
2019-03-12 11:38:48 -0400

    Adding name attribute

--- hitter.py
+++ hitter.py
@@ -1,12 +1,16 @@
 class Hitter:
 
-    def __init__(self, fb, ch, cb, sl, eye, speed):
+    def __init__(self, hitter_name, fb, ch, cb, sl): #, eye, speed):
+        self.__name = hitter_name
         self.__fastball = fb
         self.__changeup = ch
         self.__curveball = cb
         self.__slider = sl
-        self.__eye = eye
-        self.__speed = speed
+        #self.__eye = eye
+        #self.__speed = speed
+
+    def set_name(self, hitter_name):
+        self.__name = hitter_name
 
     def set_fastball(self, fb):
         self.__fastball = fb
@@ -25,6 +29,9 @@ class Hitter:
 
     def set_speed(self, speed):
         self.__speed = speed
+
+    def get_name(self):
+        return self.__name
 
     def get_fastball(self):
         return self.__fastball
